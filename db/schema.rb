fe525dbc025016f2250cc1716784fa9cff18994d
Ayatakanimeshi <ayatakathebattler2top@gmail.com>
2023-09-21 04:00:31 +0900

    マイグレーションファイルの作り直し

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_18_172651) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_20_185719) do
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "shop_id", null: false
